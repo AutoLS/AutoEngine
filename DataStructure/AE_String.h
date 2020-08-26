f3dfb196a7b2db155abb54aad24c25f8b9659ce3
AutoLS <karston_manho@hotmail.com>
2020-08-26 00:17:21 -0700

    Add files via upload

---
+++ DataStructure/AE_String.h
@@ -0,0 +1,86 @@
+#ifndef AE_STRING
+#define AE_STRING
+
+struct AE_String
+{
+	char* Data;
+	int Length;
+	int Capacity;
+};
+
+AE_String AE_Str(const char* Data)
+{
+	int Length = (int)strlen(Data);
+	
+	AE_String Str = {};
+	Str.Capacity = Length;
+	Str.Length = Length;
+	Str.Data = (char*)calloc(Str.Capacity, sizeof(char));
+	
+	for(int i = 0; i < Str.Length; ++i)
+	{
+		Str.Data[i] = Data[i];
+	}
+	
+	return Str;
+}
+
+AE_String& operator+=(AE_String& Str, const char* Data)
+{
+	int Length = (int)strlen(Data);
+	int NewLen = Length + Str.Length;
+	
+	if(Str.Capacity < NewLen)
+	{
+		Str.Capacity = NewLen * 2;
+		
+		char* NewStr = (char*)calloc(Str.Capacity, sizeof(char));
+		for(int i = 0; i < Length; ++i)
+		{
+			NewStr[i] = Str.Data[i];
+		}
+		
+		int Count = 0;
+		for(int i = Str.Length; i < NewLen; ++i)
+		{
+			NewStr[i] = Data[Count++];
+		}
+		
+		free(Str.Data);
+		
+		Str.Length = NewLen;
+		Str.Data = NewStr;
+	}
+	else
+	{
+		int Count = 0;
+		for(int i = Str.Length; i < NewLen; ++i)
+		{
+			Str.Data[i] = Data[Count++];
+		}
+		
+		Str.Length = NewLen;
+	}
+	
+	return Str;
+}
+
+AE_String operator+(AE_String StrA, AE_String StrB)
+{
+	AE_String NewStr = AE_Str(StrA.Data);
+	NewStr += StrB.Data;
+	
+	return NewStr;
+}
+
+void AE_PrintStr(AE_String* Str)
+{
+	printf("%s", Str->Data);
+}
+
+void AE_PrintStrLn(AE_String* Str)
+{
+	printf("%s\n", Str->Data);
+}
+
+#endif
